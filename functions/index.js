0f6d47192bac00909161884daa6f2a97038fd1d3
Deeksha-Singh1 <deeksha.singh1_cs19@gla.ac.in>
2021-11-22 10:22:16 +0530

    ch

--- functions/index.js
+++ functions/index.js
@@ -3,16 +3,16 @@
 const functions = require("firebase-functions");
 const express=require("express");
 const cors =require("cors");
-const { response } = require("express");
+const {response} = require("express");
 const stripe= require("stripe")("sk_test_51JyCR8SEnk29pgvema6pc2Mp7BP32hspf4WdXJyZ5WChnUCWXippqy9dUKJ03UZLUh81gjFEUp4c8wO8210BLuDj007AxUjQCT");
-//App Config
+// App Config
 const app = express();
-//Middlewares
+// Middlewares
 app.use(cors({origin: true}));
 app.use(express.json());
 
-//API routes
-app.get('/',(request, response) => response.status(200).send('hello world'))
+// API routes
+app.get("/", (request, response) => response.status(200).send("hello world"));
 
-//Listen Command
-exports.api=functions.https.onRequest(app)
+// Listen Command
+exports.api=functions.https.onRequest(app);
